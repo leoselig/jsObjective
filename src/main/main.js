827cb8420ceddb900c02e62f4932ee99b0ed3e65
Leo Selig <selig.git@gmail.com>
2013-11-30 10:54:34 +0100

    Implemented object creation and inheritance

---
+++ src/main/main.js
@@ -0,0 +1,95 @@
+/**
+ * @class Object
+ * @constructor
+ */
+var Object = function() {
+	this.init.apply(this, arguments);
+};
+
+/**
+ * @method callSuper
+ * @param method {string}
+ * @returns {*}
+ * @protected
+ * @static
+ */
+Object.callSuper = function(method) {
+	return this.constructor.__super__.constructor[method]
+		.apply(this, Array.prototype.slice.call(arguments, 1));
+}
+
+/**
+ * @method callSuper
+ * @param method {string}
+ * @returns {*}
+ * @protected
+ */
+Object.prototype.callSuper = function(method) {
+	return this.constructor.__super__[method]
+		.apply(this, Array.prototype.slice.call(arguments, 1));
+}
+
+/**
+ * @method extend
+ * @param protoProps {Object}
+ * @param staticProps {Object}
+ * @returns {Function}
+ * @protected
+ * @static
+ */
+Object.extend = function(protoProps, staticProps) {
+	var Parent = this;
+	var Child;
+
+	if(protoProps && protoProps.hasOwnProperty('constructor')) {
+		Child = protoProps.constructor;
+	} else {
+		Child = function() {
+			return Parent.apply(this, arguments);
+		};
+	}
+
+	utils.extend(Child, Parent, staticProps);
+
+	var Surrogate = function() {
+		this.constructor = Child;
+	};
+	Surrogate.prototype = Parent.prototype;
+	Child.prototype = new Surrogate;
+
+	if(protoProps) {
+		utils.extend(Child.prototype, protoProps);
+	}
+
+	Child.__super__ = Parent.prototype;
+
+	return Child;
+};
+
+/**
+ * @class utils
+ * @static
+ */
+var utils = {
+
+	/**
+	 * @method extend
+	 * @param base {Object}
+	 * @returns {Object}
+	 * @public
+	 */
+	extend: function(base) {
+		for(var i = 1; i < arguments.length; i++) {
+			for(var j in arguments[i]) {
+				if(!arguments[i][j].hasOwnProperty(j)) {
+					base[j] = arguments[i][j];
+				}
+			}
+		}
+		return base;
+	}
+}
+
+module.exports = {
+	Object: Object
+}
